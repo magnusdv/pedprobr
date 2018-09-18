3a1b4838d11f011118bc0c7fe71a52c22959882c
magnusdv <magnusdv@medisin.uio.no>
2018-09-18 15:08:36 +0200

    Add utility `hasStationaryModel()`
    * perhaps move this to `pedmut`

--- R/mutationModel.R
+++ R/mutationModel.R
@@ -3,3 +3,17 @@
 #' @importFrom pedmut mutationModel
 #' @export
 pedmut::mutationModel
+
+
+
+#' @importFrom pedmut isStationary
+hasStationaryModel = function(m) {
+  mut = mutmod(m)
+  if(is.null(mut)) return(TRUE)
+
+  sexEq = attr(mut, 'sexEqual')
+  afr = afreq(m)
+
+  isStationary(mut$male, afr) &&
+    (isTRUE(sexEq) || isStationary(mut$male, afr))
+}
