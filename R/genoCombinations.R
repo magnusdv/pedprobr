778604b3b8da61ef9f8778bfc2d5a83d6ec51166
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2019-10-21 11:21:13 +0200

    Simplify `genoCombinations()` and improve docs

--- R/genoCombinations.R
+++ R/genoCombinations.R
@@ -1,23 +1,28 @@
 #' Genotype combinations
 #'
-#' Auxiliary functions computing possible genotype combinations in a pedigree.
-#' These are not normally intended for end users.
-#'
+#' Returns the possible genotype combinations in a pedigree, given partial
+#' marker data. This function is mainly for internal use.
 #'
 #' @param x a [ped()] object.
 #' @param partialmarker a [marker()] object compatible with `x`.
 #' @param ids a vector with ID labels of one or more pedigree members.
-#' @param make.grid a logical. If FALSE, a list is returned, otherwise the
-#'   internal `fastGrid()` is applied to the list before returning it.
+#' @param make.grid a logical indicating if the result should be simplified to a
+#'   matrix.
+#'
+#' @return If `make.grid = FALSE` (the default) the function returns a list of
+#'   integer vectors, one vector for each element of `ids`. Each integer
+#'   represents a genotype, in the form of a row number of the matrix
+#'   `allGenotypes(n)`, where `n` is the number of alleles of the marker.
+#'
+#'   If `make.grid = TRUE`, the cartesian product of the vectors is taken,
+#'   resulting in a matrix with one column for each element of `ids`.
 #'
 #' @export
 genoCombinations = function(x, partialmarker, ids, make.grid = T) {
     int.ids = internalID(x, ids)
-    nall = nAlleles(partialmarker)
     mutations = allowsMutations(partialmarker)
-    chrom = if (isXmarker(partialmarker)) "X" else "AUTOSOMAL"
 
-    allg = allGenotypes(nall)
+    allg = allGenotypes(nAlleles(partialmarker))
     allgRef = 1000 * (allg[, 1] + allg[, 2]) + abs(allg[, 1] - allg[, 2])
 
     matchRefRows = function(genomatr) {
@@ -27,16 +32,19 @@ genoCombinations = function(x, partialmarker, ids, make.grid = T) {
       sort.int(unique.default(match(1000 * (row1 + row2) + abs(row1 - row2), allgRef)))
     }
 
-    switch(chrom, AUTOSOMAL = {
-        glist = .buildGenolist(x, partialmarker, eliminate = ifelse(mutations, 0, 100))
-        if (attr(glist, "impossible")) stop2("Impossible partial marker")
-        rows = lapply(glist[int.ids], matchRefRows)
-    }, X = {
-        SEX = x$SEX
-        glist = .buildGenolistX(x, partialmarker, eliminate = ifelse(mutations, 0, 100))
-        if (attr(glist, "impossible")) stop2("Impossible partial marker")
-        rows = lapply(int.ids, function(i) switch(SEX[i], glist[[i]], matchRefRows(glist[[i]])))
-    })
+    if (isXmarker(partialmarker)) {
+      SEX = x$SEX
+      glist = .buildGenolistX(x, partialmarker, eliminate = ifelse(mutations, 0, 100))
+      if (attr(glist, "impossible")) stop2("Impossible partial marker")
+      rows = lapply(int.ids, function(i) switch(SEX[i], glist[[i]], matchRefRows(glist[[i]])))
+    }
+    else {
+      glist = .buildGenolist(x, partialmarker, eliminate = ifelse(mutations, 0, 100))
+      if (attr(glist, "impossible"))
+        stop2("Impossible partial marker")
+      rows = lapply(glist[int.ids], matchRefRows)
+    }
+
     if (make.grid)
         fastGrid(rows) else rows
 }
